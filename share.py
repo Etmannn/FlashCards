83cd9a5a6afd600fc83254a317ba41cfdce5649c
Etmannn <128926337+Etmannn@users.noreply.github.com>
2023-11-11 14:42:37 +0000

    Fixed directory problem, works with Replit.

--- share.py
+++ share.py
@@ -11,7 +11,7 @@ with open(file, "rb") as f:
     Quiz_Details = pickle.load(f)
 
 shareto = input("Input name of user to share to: ")
-sharefile = f"userfiles\\{shareto}\\shared"
+sharefile = f"userfiles/{shareto}/shared"
 
 if shareto in os.listdir("userfiles"):
     shutil.copy(file, sharefile)
